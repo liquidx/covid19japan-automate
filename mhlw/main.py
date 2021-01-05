dec0d544a632a8b4cb67f22ae519bb2674115f3e
Alastair Tse <alastair@liquidx.net>
2021-01-05 17:52:44 +0900

    Move mhlw service to it's own directory

--- mhlw/main.py
+++ mhlw/main.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+import json
 import mhlw
 
 app = Flask(__name__)
@@ -14,11 +15,14 @@ def hello_world():
 @app.route('/reporturl')
 def report_url():
   url = mhlw.getLatestCovidReport(mhlw.DEFAULT_MHLW_INDEX_URL)
-  return 'Report URL: {}'.format(url)
+  return json.dumps({'result': {
+    'url': url
+  })
 
 @app.route('/mhlw/today')
 def report_today():
-  return mhlw.reportToday(True)
+  result = mhlw.reportToday(True)
+  return json.dumps({'result': result})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
