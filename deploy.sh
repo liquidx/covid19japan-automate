f0a084a599d240f9cc35767e93c5116607e04e14
Alastair Tse <alastair@liquidx.net>
2021-04-13 22:01:52 +0900

    Update deploy script to give required region

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ SERVICE_ID=covid19japan-auto
 cd python
 gcloud run deploy  \
   --project ${PROJECT_ID} \
+  --region asia-northeast1 \
   --image gcr.io/${PROJECT_ID}/${SERVICE_ID} \
   --platform managed \
   --allow-unauthenticated \
