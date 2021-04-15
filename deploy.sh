a7d883cc68da13e93c77ec148a992d4f4b247906
Alastair Tse <alastair@liquidx.net>
2021-04-15 11:10:26 +0900

    Deploy to us-central1 instead of asia

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ SERVICE_ID=covid19japan-auto
 cd python
 gcloud run deploy  \
   --project ${PROJECT_ID} \
-  --region asia-northeast1 \
+  --region us-central1 \
   --image gcr.io/${PROJECT_ID}/${SERVICE_ID} \
   --platform managed \
   --allow-unauthenticated \
